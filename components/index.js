ccc7814fdb1fdbf08aedf8dde0cf67bd32fd9cf8
Gautham Vanjre <vanjregautham@gmail.com>
2023-06-11 12:59:25 +0530

    initial commit

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import DetailsHeader from "./DetailsHeader";
+import {SubInfo} from "./SubInfo";
+
+export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton,DetailsBid,DetailsDesc,DetailsHeader,SubInfo };
